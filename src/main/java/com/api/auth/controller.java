71a8b508df7be2b0287ec6b98ca31eb7053774cf
pratyush <probohidar2016@gmail.com>
2024-01-29 21:50:28 +0530

    config

--- src/main/java/com/api/auth/controller.java
+++ src/main/java/com/api/auth/controller.java
@@ -1,6 +1,5 @@
 package com.api.auth;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,13 +22,10 @@ public class controller {
     public String login(){
         System.out.println("token: ");
         return "token: ";
-
     }
-
 
     @GetMapping("/test")
     public String test() {
-
         return "server is live...";
     }
 
